8e1f7a20ece3506ab7b33069fb6d2319972fa28a
Nessie-13 <121945825+Nessie-13@users.noreply.github.com>
2023-12-04 21:27:43 +0000

    Add files via upload
    Updated dart

--- elemental_project/lib/elemental_project.dart
+++ elemental_project/lib/elemental_project.dart
@@ -1,4 +1,12 @@
+import 'dart:async';
+
+import 'package:elemental_project/earth_levels/e01.dart';
 import 'package:flame/game.dart';
 
-class ElementalProject extends FlameGame {}
-
+class ElementalProject extends FlameGame {
+  @override
+  FutureOr<void> onLoad() {
+    add(E01());
+    return super.onLoad();
+  }
+}
