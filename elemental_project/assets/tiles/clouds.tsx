6c209686a52541a1a51d648f9e4ef147321790d2
Nessie-13 <121945825+Nessie-13@users.noreply.github.com>
2023-12-04 21:19:09 +0000

    Add files via upload
    Tiled files

---
+++ elemental_project/assets/tiles/clouds.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="clouds" tilewidth="16" tileheight="16" tilecount="234" columns="18">
+ <image source="Background/3 - Cloud_cover_1.png" width="288" height="208"/>
+</tileset>
---
+++ elemental_project/assets/tiles/earth entity 01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="earth entity 01" tilewidth="16" tileheight="16" tilecount="170" columns="17">
+ <image source="Terrain/earth terrain.png" width="272" height="160"/>
+</tileset>
